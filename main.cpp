f384e696ef7681921f30055b0133f1ab1547e872
Wei Fei <weifei6441@gmail.com>
2017-10-22 18:29:16 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**********************************************************
+*Name: Wei Fei
+*KUID: 2538810
+*Date: 02/12/14
+*File: main.cpp
+*Course: EECS560 - Data Structure
+*Project: Lab3 - Closed Hashing with Open Addressing
+***********************************************************/
+
+#include<iostream>
+#include "HashOperation.h"
+
+int main(int args, char* argv[])
+{
+	HashOperation myHash;
+	myHash.finalOutput();
+	system("pause");
+	return 0;
+}
