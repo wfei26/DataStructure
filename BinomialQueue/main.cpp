432052ded2d7eb2980d6686f8c4f01137cd1e9a6
Wei Fei <weifei6441@gmail.com>
2017-11-01 14:18:58 -0700

    Add files via upload

---
+++ BinomialQueue/main.cpp
@@ -0,0 +1,21 @@
+/********************************************
+*Name: Wei Fei
+*KUID: 2538810
+*Date: 04/16/15
+*File: main.cpp
+*Course: EECS560 - Data Structure
+*Project: Lab9 - Binomial Queue
+*********************************************/
+
+#include<iostream>
+#include "BinomialQueueOperation.h"
+
+using namespace std;
+
+int main()
+{
+	BinomialQueueOperation myBinomialQueue;
+	myBinomialQueue.finalOutput();
+	system("pause");
+	return 0;
+}
