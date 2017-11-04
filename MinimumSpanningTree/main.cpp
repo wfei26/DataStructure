552b5403e7dd72c68c8a84aecd9d9bd1e9819e13
Wei Fei <weifei6441@gmail.com>
2017-11-04 00:19:14 -0700

    Add files via upload

---
+++ MinimumSpanningTree/main.cpp
@@ -0,0 +1,21 @@
+/*****************************************
+*Name: Wei Fei
+*KUID: 2538810
+*Date: 05/03/15
+*File: main.cpp
+*Course: EECS560 - Data Structure
+*Project: Lab9 - Minimum Spanning Tree
+******************************************/
+
+#include<iostream>
+#include "SpanningTreeOperation.h"
+
+using namespace std;
+
+int main()
+{
+	SpanningTreeOperation mySpanningTree;
+	mySpanningTree.finalOutput();
+	system("pause");
+	return 0;
+}
