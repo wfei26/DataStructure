0080a70a871d93103f968a22991179850f2a8a74
Wei Fei <weifei6441@gmail.com>
2017-11-09 15:51:37 -0800

    Add files via upload

---
+++ MinMaxHeap/main.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+*Name: Wei Fei
+*KUID: 2538810
+*Date: 03/30/15
+*File: main.cpp
+*Course: EECS560 - Data Structure
+*Project: Lab7 - Min-Max Heap
+*****************************************/
+
+#include <iostream>
+#include "MinMaxHeapOperation.h"
+using namespace std;
+
+void main()
+{
+	MinMaxHeapOperation myMinMaxHeap;
+	myMinMaxHeap.finalOutput();
+	system("pause");
+}
