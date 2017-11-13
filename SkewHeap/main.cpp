321bafdc74e98fd8891c20b1291d2f1b680ca5ff
Wei Fei <weifei6441@gmail.com>
2017-11-13 01:51:21 -0800

    Update main.cpp

--- SkewHeap/main.cpp
+++ SkewHeap/main.cpp
@@ -4,7 +4,7 @@
 *Date: 04/02/15
 *File: main.cpp
 *Course: EECS560 - Data Structure
-*Project: Lab8 - Comparing the Performance of Leftist Heap and Skew Heap
+*Project: Lab8 - Comparing the Performance Difference between Leftist Heap and Skew Heap
 *************************************************************************/
 
 #include<iostream>
