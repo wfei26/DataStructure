d21455fa8e4b1418976156c5e5a885899f2bbc94
Wei Fei <weifei6441@gmail.com>
2017-11-19 16:01:19 -0800

    Add files via upload

---
+++ TimingComparison/HeapNode.cpp
@@ -0,0 +1,98 @@
+/********************************************************************************************
+*Name: Wei Fei
+*KUID: 2538810
+*Date: 04/23/15
+*File: HeapNode.cpp
+*Course: EECS560 - Data Structure
+*Project: Lab10 - Comparing the performance of Binomial queue with Leftist Heap and Skew Heap
+*********************************************************************************************/
+
+#include "HeapNode.h"
+
+HeapNode::HeapNode()
+{
+	item = 0;
+	rank = 0;
+	leftChild = NULL;
+	rightChild = NULL;
+}
+
+
+HeapNode::HeapNode(int newItem)
+{
+	item = newItem;
+	rank = 0;
+	leftChild = NULL;
+	rightChild = NULL;
+}
+
+
+HeapNode::HeapNode(int newItem, int newRank)
+{
+	item = newItem;
+	rank = newRank;
+	leftChild = NULL;
+	rightChild = NULL;
+}
+
+
+HeapNode::HeapNode(int newItem, HeapNode* newLeftChild, HeapNode* newRighrChild)
+{
+	item = newItem;
+	rank = 0;
+	leftChild = newLeftChild;
+	rightChild = newRighrChild;
+}
+
+
+HeapNode::~HeapNode()
+{
+}
+
+
+int HeapNode::getItem()
+{
+	return item;
+}
+
+
+void HeapNode::setItem(const int newItem)
+{
+	item = newItem;
+}
+
+
+int HeapNode::getRank()
+{
+	return rank;
+}
+
+
+void HeapNode::setRank(const int newRank)
+{
+	rank = newRank;
+}
+
+
+HeapNode* HeapNode::getLeftChild()
+{
+	return leftChild;
+}
+
+
+void HeapNode::setLeftChild(HeapNode* newLeftChild)
+{
+	leftChild = newLeftChild;
+}
+
+
+HeapNode* HeapNode::getRightChild()
+{
+	return rightChild;
+}
+
+
+void HeapNode::setRightChild(HeapNode* newRightChild)
+{
+	rightChild = newRightChild;
+}
